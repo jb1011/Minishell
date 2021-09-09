8dc24b76ae67da7c0ca3542a0c85abadd0e5df23
LouGel <l.gelinet@protonmail.ch>
2021-09-09 11:16:32 +0200

    Forgot to push

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: lgelinet <lgelinet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 16:31:11 by lgelinet          #+#    #+#             */
-/*   Updated: 2021/09/03 15:59:02 by lgelinet         ###   ########.fr       */
+/*   Updated: 2021/09/07 17:08:13 by lgelinet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,8 @@ int main(int argc, char **argv)
 	{
 		begin(&all);
 		parse_line(&all);
-		 printf("%s\n", all.w_line[0][1]);
-		// _echo(0, all.w_line[0], 0, 0);
+		//printf("%s\n", all.w_line[0][1]);
+		_echo(0, all.w_line[0], 0, 0);
 		// _cd("~");
 	}
 }
